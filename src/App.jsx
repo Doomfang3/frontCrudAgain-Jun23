d01e0557ad575bfaf13c8439686a8a30990b9575
Doomfang3 <saubinmathieu@gmail.com>
2023-08-02 10:30:55 +0200

    Add page to show all students

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import AllStudentsPage from './pages/AllStudentsPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<h1>Home page</h1>} />
+        <Route path='/students' element={<AllStudentsPage />} />
 
         <Route path='*' element={<h1>404 page</h1>} />
       </Routes>
