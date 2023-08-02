4213f40f4ddb888747e4efa2682ed8c9b764bbe4
Doomfang3 <saubinmathieu@gmail.com>
2023-08-02 11:01:47 +0200

    Add student details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import AllStudentsPage from './pages/AllStudentsPage'
+import StudentDetailsPage from './pages/StudentDetailsPage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<h1>Home page</h1>} />
         <Route path='/students' element={<AllStudentsPage />} />
+        <Route path='/students/:studentId' element={<StudentDetailsPage />} />
 
         <Route path='*' element={<h1>404 page</h1>} />
       </Routes>
